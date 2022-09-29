e184b383aad4a4458ea2c5929a2187df57231904
zhangkx <1099253732@qq.com>
2022-09-29 11:50:16 +0800

    feat:公司信息完成

--- src/api/setting.js
+++ src/api/setting.js
@@ -6,6 +6,11 @@ export const getRoleList = (params) => {
   })
 }
 
+/**
+ * 添加接口
+ * @param {} data
+ * @returns
+ */
 export const addRole = (data) => {
   return request({
     url: '/sys/role',
@@ -14,10 +19,33 @@ export const addRole = (data) => {
   })
 }
 
+/**
+ * 编辑接口
+ * @param {Object} data
+ * @returns
+ */
 export function updateRole(data) {
   return request({
     url: `/sys/role/${data.id}`,
     data,
     method: 'put'
+  })
+}
+
+/**
+ * 删除接口
+ * @param {Number} id
+ * @returns
+ */
+export function deleteRole(id) {
+  return request({
+    url: `/sys/role/${id}`,
+    method: 'delete'
+  })
+}
+
+export function getCompanyInfo(companyId) {
+  return request({
+    url: `/company/${companyId}`
   })
 }
