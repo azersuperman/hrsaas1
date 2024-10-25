155347c20bbfd9d9ff78600f9676e27d17c7beca
zhangkx <1099253732@qq.com>
2024-10-25 23:57:30 +0800

    4

--- src/views/404.vue
+++ src/views/404.vue
@@ -5,7 +5,7 @@
         <img
           class="pic-404__parent"
           src="@/assets/404_images/404.png"
-          alt="404"
+          alt="409"
         />
         <img
           class="pic-404__child left"
